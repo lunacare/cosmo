69f45bdeef14db17e6562a3e8ac56ebd6f2e64b3
Korey <korey.gam@gmail.com>
2025-03-19 12:07:31 -0600

    Formatting

--- router/build-and-deploy.sh
+++ router/build-and-deploy.sh
@@ -6,15 +6,15 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 cd "$ROOT_DIR"
 
 if [[ $# -ne 1 ]]; then
-        echo "1 --- BUILD_AS_TEST - 'true' or 'false'"
-        exit 1
+  echo "1 --- BUILD_AS_TEST - 'true' or 'false'"
+  exit 1
 fi
 BUILD_AS_TEST=$1
 
 ECR_REPO="lunacare-cosmo-router"
 ECR_URL="836236105554.dkr.ecr.us-west-2.amazonaws.com/$ECR_REPO"
 
-VERSION=$( grep -Eo '\[[0-9]+\.[0-9]+\.[0-9]+\]' ./router/CHANGELOG.md | tr -d '[]' | sort -V | tail -n1 )
+VERSION=$(grep -Eo '\[[0-9]+\.[0-9]+\.[0-9]+\]' ./router/CHANGELOG.md | tr -d '[]' | sort -V | tail -n1)
 VERSION="$VERSION-lunacare-$(head -n 1 ./router/VERSION-LUNACARE)"
 VERSION_TAG="$ECR_URL:$VERSION"
 LATEST_TAG="$ECR_URL:latest"
@@ -22,47 +22,47 @@ LATEST_TEST_TAG="$ECR_URL:latest-test"
 
 GITHUB_ACTIONS="${GITHUB_ACTIONS:-"false"}"
 if [[ "$GITHUB_ACTIONS" == "false" ]]; then
-        CMD_DESCRIBE_IMAGES="aws ecr describe-images --repository-name $ECR_REPO --image-ids imageTag=$VERSION --profile live"
+  CMD_DESCRIBE_IMAGES="aws ecr describe-images --repository-name $ECR_REPO --image-ids imageTag=$VERSION --profile live"
 else
-        CMD_DESCRIBE_IMAGES="aws ecr describe-images --repository-name $ECR_REPO --image-ids imageTag=$VERSION"
+  CMD_DESCRIBE_IMAGES="aws ecr describe-images --repository-name $ECR_REPO --image-ids imageTag=$VERSION"
 fi
 
 # Execute the command and capture the error message
 echo "running command: $CMD_DESCRIBE_IMAGES"
 if output=$(eval "$CMD_DESCRIBE_IMAGES" 2>&1); then
-    echo "VERSION $VERSION_TAG already exists. Exiting without building images"
-    exit 1
+  echo "VERSION $VERSION_TAG already exists. Exiting without building images"
+  exit 1
 else
-    # Check for specific error messages
-    if [[ $output == *"ImageNotFoundException"* ]]; then
-        echo "Tag for VERSION $VERSION not found, proceeding to build it."
-    elif [[ $output == *"InvalidParameterException"* ]]; then
-        echo "Error: Invalid parameter when describing ecr images $VERSION."
-        exit 1
-    else
-        echo "An unexpected error occurred: $output"
-        exit 1
-    fi
+  # Check for specific error messages
+  if [[ $output == *"ImageNotFoundException"* ]]; then
+    echo "Tag for VERSION $VERSION not found, proceeding to build it."
+  elif [[ $output == *"InvalidParameterException"* ]]; then
+    echo "Error: Invalid parameter when describing ecr images $VERSION."
+    exit 1
+  else
+    echo "An unexpected error occurred: $output"
+    exit 1
+  fi
 fi
 
 if [ "$BUILD_AS_TEST" == "true" ]; then
-        TAG_ARGS="-t ${LATEST_TEST_TAG}"
-        TAGS_TO_PUSH="${LATEST_TEST_TAG}"
+  TAG_ARGS="-t ${LATEST_TEST_TAG}"
+  TAGS_TO_PUSH="${LATEST_TEST_TAG}"
 else
-        TAG_ARGS="-t ${LATEST_TAG} -t ${VERSION_TAG}"
-        TAGS_TO_PUSH="${LATEST_TAG} ${VERSION_TAG}"
+  TAG_ARGS="-t ${LATEST_TAG} -t ${VERSION_TAG}"
+  TAGS_TO_PUSH="${LATEST_TAG} ${VERSION_TAG}"
 fi
 
-IFS=' ' read -r -a TAGS_TO_PUSH_ARRAY <<< "$TAGS_TO_PUSH"
+IFS=' ' read -r -a TAGS_TO_PUSH_ARRAY <<<"$TAGS_TO_PUSH"
 for TAG in "${TAGS_TO_PUSH_ARRAY[@]}"; do
-        echo "Set tag to push: $TAG"
+  echo "Set tag to push: $TAG"
 done
 
 export ECR_REPO="$ECR_REPO"
 export TAG_ARGS="$TAG_ARGS"
 export TAGS_TO_PUSH="$TAGS_TO_PUSH"
 
-IFS=' ' read -r -a TAG_ARGS_ARRAY <<< "$TAG_ARGS"
+IFS=' ' read -r -a TAG_ARGS_ARRAY <<<"$TAG_ARGS"
 
 # Create a new builder instance if it doesn't exist
 BUILDER_NAME="cosmo-multiplatform-builder"
@@ -83,29 +83,33 @@ DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 # Build and push the images
 
 # For local development
-# docker buildx build \
-#         --platform linux/arm64 \
-#         --build-arg TARGETOS=linux \
-#         --build-arg TARGETARCH=arm64 \
-#         --build-arg COMMIT_SHA=${COMMIT_SHA} \
-#         --build-arg DATE=${DATE} \
-#         --build-arg VERSION=${VERSION} \
-#         "${TAG_ARGS_ARRAY[@]}" \
-#         --push \
-#         -f ./router/custom-luna.Dockerfile \
-#         --progress plain \
-#         ./router
+if [[ "$GITHUB_ACTIONS" == "false" ]]; then
+  docker buildx build \
+    --platform linux/arm64 \
+    --build-arg TARGETOS=linux \
+    --build-arg TARGETARCH=arm64 \
+    --build-arg COMMIT_SHA=${COMMIT_SHA} \
+    --build-arg DATE=${DATE} \
+    --build-arg VERSION=${VERSION} \
+    "${TAG_ARGS_ARRAY[@]}" \
+    --push \
+    -f ./router/custom-luna.Dockerfile \
+    --progress plain \
+    ./router
+fi
 
-docker buildx build \
-        --platform linux/amd64 \
-        --build-arg TARGETOS=linux \
-        --build-arg TARGETARCH=amd64 \
-        --build-arg COMMIT_SHA=${COMMIT_SHA} \
-        --build-arg DATE=${DATE} \
-        --build-arg VERSION=${VERSION} \
-        "${TAG_ARGS_ARRAY[@]}" \
-        --push \
-        -f ./router/custom-luna.Dockerfile \
-        --progress plain \
-        ./router
-
+# For GitHub Actions
+if [[ "$GITHUB_ACTIONS" == "true" ]]; then
+  docker buildx build \
+    --platform linux/amd64 \
+    --build-arg TARGETOS=linux \
+    --build-arg TARGETARCH=amd64 \
+    --build-arg COMMIT_SHA=${COMMIT_SHA} \
+    --build-arg DATE=${DATE} \
+    --build-arg VERSION=${VERSION} \
+    "${TAG_ARGS_ARRAY[@]}" \
+    --push \
+    -f ./router/custom-luna.Dockerfile \
+    --progress plain \
+    ./router
+fi
